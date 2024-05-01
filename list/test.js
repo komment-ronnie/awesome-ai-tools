4846432df20c5fd415fa48f03be60a4f90986015
komment-ronnie <153918503+komment-ronnie@users.noreply.github.com>
2024-05-01 11:10:26 +0000

    Remote commit 1

--- list/test.js
+++ list/test.js
@@ -1,4 +1,20 @@
 const fn = () => {
+    console.log("1");
+}
+
+const fn = () => {
+    console.log("3");
+}
+
+const fn = () => {
+    console.log("3");
+}
+
+const fn = () => {
+    console.log("3");
+}
+
+const fn = () => {
     console.log("3");
 }
 
